b3496d86ffc94829b03d73a063973cef8f944137
omar wheed <wheedomar88@gmail.com>
2024-07-26 10:42:14 -0700

    connect flutter project with firebase and fix bugs

---
+++ lib/asroo_store_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AsrooStoreApp extends StatelessWidget {
+  const AsrooStoreApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("omar"),
+      ),
+    );
+  }
+}
