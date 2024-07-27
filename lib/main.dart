b0129a5e882898bb085b4c922fe95d75c9116047
omar wheed <wheedomar88@gmail.com>
2024-07-27 07:31:18 -0700

    env created done and device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,34 @@
 import 'package:asroo_store/asroo_store_app.dart';
+import 'package:asroo_store/core/app/env_variable.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //env
+  await EnvVariable.instance.init(type: EnvTypeEnum.dev);
+  //firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp( ));
+  //portrait
+  await SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]).then(
+    (_) {
+      runApp(const MyApp());
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home:  AsrooStoreApp(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+      home: const AsrooStoreApp(),
     );
   }
 }
