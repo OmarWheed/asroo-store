c60e845db767b3a643985da7a66118ab630e3bc7
omar wheed <wheedomar88@gmail.com>
2024-08-02 08:35:05 -0700

    custom_widget

--- lib/core/style/fonts/font_family_helper.dart
+++ lib/core/style/fonts/font_family_helper.dart
@@ -3,4 +3,12 @@ class FontFamilyHelper {
 
   static const String cairoArabic = 'Cairo';
   static const String poppinsEnglish = 'Poppins';
+
+  static String getLocalizedFontFamily() {
+    String currentFont = 'ar';
+    if (currentFont == 'ar') {
+      return cairoArabic;
+    }
+    return poppinsEnglish;
+  }
 }
--- lib/core/style/theme/app_theme.dart
+++ lib/core/style/theme/app_theme.dart
@@ -1,5 +1,6 @@
 import 'package:asroo_store/core/style/colors/colors_dark.dart';
 import 'package:asroo_store/core/style/colors/colors_light.dart';
+import 'package:asroo_store/core/style/fonts/font_family_helper.dart';
 import 'package:asroo_store/core/style/theme/color_extension.dart';
 import 'package:asroo_store/core/style/theme/image_extension.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +9,12 @@ ThemeData themeDark() {
   return ThemeData(
     scaffoldBackgroundColor: ColorsDark.mainColor,
     extensions: const <ThemeExtension<dynamic>>[MyColors.dark, MyImage.dark],
-   
+    textTheme: TextTheme(
+      displaySmall: TextStyle(
+          fontSize: 14,
+          color: ColorsDark.white,
+          fontFamily: FontFamilyHelper.getLocalizedFontFamily()),
+    ),
   );
 }
 
@@ -16,5 +22,11 @@ ThemeData themeLight() {
   return ThemeData(
     scaffoldBackgroundColor: ColorsLight.mainColor,
     extensions: const <ThemeExtension<dynamic>>[MyColors.light, MyImage.light],
+    textTheme: TextTheme(
+      displaySmall: TextStyle(
+          fontSize: 14,
+          color: ColorsLight.black,
+          fontFamily: FontFamilyHelper.getLocalizedFontFamily()),
+    ),
   );
 }
